43631243bc7d4ab1d2929756ae88fe2eea00210c
mosaic-baby <mosaicu0baby@gmail.com>
2026-06-22 11:43:19 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1.App2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(line1.Text))
+            {
+                // Добавляем текст из текстового поля в список
+                line2.Items.Add(line1.Text);
+
+                // Очищаем текстовое поле после добавления (по желанию)
+                line1.Clear();
+            }
+        }
+
+        
+
+
+    }
+}
